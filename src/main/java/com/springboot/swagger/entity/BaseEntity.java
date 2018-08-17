492828b63c1c492491a6ff0834f50928d595da86
lixiaole <franky8805@gmail.com>
2018-08-17 18:00:30 +0800

    修改实体类 重构部分代码

---
+++ src/main/java/com/springboot/swagger/entity/BaseEntity.java
@@ -0,0 +1,90 @@
+package com.springboot.swagger.entity;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import javax.persistence.Column;
+import javax.persistence.MappedSuperclass;
+import javax.persistence.Version;
+import java.io.Serializable;
+import java.time.ZonedDateTime;
+
+/**
+ * @author lixiaole
+ * @date 2018/8/1716:36
+ */
+@MappedSuperclass //使用该注解 使其类不映射到数据库表(不在数据库中创建表  但其属性字段都将在子类数据库字段中)
+public abstract class BaseEntity implements Serializable {
+    //设置序列化ID
+    private static final long serialVersionUID = 6667979858215354999L;
+
+    @Version
+    @JsonIgnore
+    @Column(columnDefinition = "integer Default 0 ",nullable = false)
+    private Long version;
+
+    @CreatedBy
+    @JsonIgnore
+    @Column(name = "create_by" ,length = 50 ,updatable = false)
+    private String CreateBy;
+
+    @CreatedDate
+    @JsonIgnore
+    @Column(name = "create_date")
+    private ZonedDateTime createDate = ZonedDateTime.now();
+
+
+    @LastModifiedBy
+    @JsonIgnore
+    @Column(name = "update_by", length = 50)
+    private String lastModifiedBy;
+
+    @LastModifiedDate
+    @Column(name = "update_date")
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    private ZonedDateTime lastModifiedDate = ZonedDateTime.now();
+
+    public Long getVersion() {
+        return version;
+    }
+
+    public void setVersion(Long version) {
+        this.version = version;
+    }
+
+    public String getCreateBy() {
+        return CreateBy;
+    }
+
+    public void setCreateBy(String createBy) {
+        CreateBy = createBy;
+    }
+
+    public ZonedDateTime getCreateDate() {
+        return createDate;
+    }
+
+    public void setCreateDate(ZonedDateTime createDate) {
+        this.createDate = createDate;
+    }
+
+    public String getLastModifiedBy() {
+        return lastModifiedBy;
+    }
+
+    public void setLastModifiedBy(String lastModifiedBy) {
+        this.lastModifiedBy = lastModifiedBy;
+    }
+
+    public ZonedDateTime getLastModifiedDate() {
+        return lastModifiedDate;
+    }
+
+    public void setLastModifiedDate(ZonedDateTime lastModifiedDate) {
+        this.lastModifiedDate = lastModifiedDate;
+    }
+}
--- src/main/java/com/springboot/swagger/entity/User.java
+++ src/main/java/com/springboot/swagger/entity/User.java
@@ -1,20 +1,30 @@
 package com.springboot.swagger.entity;
 
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
 
 import javax.persistence.*;
 import java.io.Serializable;
+import java.time.ZonedDateTime;
 
 @Entity
-@Table(name = "t_author_jpa")
+@Table(name = "t_user")
 @ApiModel(value = "用户实体类")
-public class User implements Serializable {
+public class User extends BaseEntity implements Serializable {
+    private static final long serialVersionUID = 7484593197505800799L;
+
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)//IDENTITY：主键由数据库自动生成（主要是自动增长型）
     @ApiModelProperty(value = "用户id")
     private long id;
+
+    @ApiModelProperty(value = "用户名")
+    private String userName;
+
+    @ApiModelProperty(value = "密码")
+    private String password;
 
     @Column(name = "real_name")
     @ApiModelProperty(value = "姓名")
@@ -24,12 +34,36 @@ public class User implements Serializable {
     @ApiModelProperty(value = "昵称")
     private String nickName;
 
+    @ApiModelProperty(value = "性别")
+    private Integer sex;
+
+    @Column(name = "register_date")
+    @ApiModelProperty(value = "注册日期")
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    private ZonedDateTime registerDate = ZonedDateTime.now();
+
     public long getId() {
         return id;
     }
 
     public void setId(long id) {
         this.id = id;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
     }
 
     public String getRealName() {
@@ -48,12 +82,32 @@ public class User implements Serializable {
         this.nickName = nickName;
     }
 
+    public Integer getSex() {
+        return sex;
+    }
+
+    public void setSex(Integer sex) {
+        this.sex = sex;
+    }
+
+    public ZonedDateTime getRegisterDate() {
+        return registerDate;
+    }
+
+    public void setRegisterDate(ZonedDateTime registerDate) {
+        this.registerDate = registerDate;
+    }
+
     @Override
     public String toString() {
         return "User{" +
                 "id=" + id +
+                ", userName='" + userName + '\'' +
+                ", password='" + password + '\'' +
                 ", realName='" + realName + '\'' +
                 ", nickName='" + nickName + '\'' +
+                ", sex=" + sex +
+                ", registerDate=" + registerDate +
                 '}';
     }
 }
